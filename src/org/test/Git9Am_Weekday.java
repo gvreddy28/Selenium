9e62779ed8bfe2c8ee0b874212ce1828d21af119
joshua <joshuadurairaj.12@gmail.com>
2019-03-18 12:52:22 +0530

    sample

---
+++ src/org/test/Git9Am_Weekday.java
@@ -0,0 +1,23 @@
+package org.test;
+
+
+public class Git9Am_Weekday {
+	
+	
+public void empid() {
+
+System.out.println("12345");
+
+}
+
+public void Name() {
+	
+System.out.println("gv reddy");
+
+}
+public static void main(String[] args) {
+	Git9Am_Weekday c= new Git9Am_Weekday();
+	c.empid();
+	c.Name();
+}
+}
